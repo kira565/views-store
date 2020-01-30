e49cb0265af139689e0bf5e43a0b2f59f7aec0c7
Kirill Kagdin <rlyiamberserker@gmail.com>
2020-01-30 19:17:46 +0300

    + viewsStoreModel export

--- projectRoot/stores/Project/views/ViewStore.ts
+++ projectRoot/stores/Project/views/ViewStore.ts
@@ -2,12 +2,9 @@ import createBaseTableModel from "../createBaseTableModel";
 import BaseRepository from "../BaseRepository";
 import { ViewModel } from "./ViewModel";
 import { ViewEntity } from "./ViewEntity";
-import { getConnection, createConnection, getCustomRepository, Repository, EntityRepository } from "typeorm";
-import { LayerEntity, LisObjectEntity } from "../../internal";
-import { Instance, getSnapshot, SnapshotIn } from "mobx-state-tree";
+import { EntityRepository } from "typeorm";
+import { Instance} from "mobx-state-tree";
 
 @EntityRepository(ViewEntity)
 class ViewStoreRepo extends BaseRepository<ViewEntity, Instance<typeof ViewModel> & Record<string, any>> {}
-
-const viewStoreModel = createBaseTableModel(ViewModel, ViewStoreRepo);
-export const viewStore = viewStoreModel.create();
+export const viewStoreModel = createBaseTableModel(ViewModel, ViewStoreRepo);
