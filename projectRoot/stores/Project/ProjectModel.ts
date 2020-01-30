52af34a040947dafd08b6c0cc60d1bc9e56ca706
Kirill Kagdin <rlyiamberserker@gmail.com>
2020-01-30 19:59:08 +0300

    + ProjectModel export

---
+++ projectRoot/stores/Project/ProjectModel.ts
@@ -0,0 +1,36 @@
+import { types } from "mobx-state-tree";
+import { createConnection, ViewEntity, getManager } from "typeorm";
+import { LayerEntity, LisObjectEntity } from "../internal";
+import { ViewStoreModel } from "./views/ViewStore";
+
+export const DEFAULT_VIEW = {
+	id: 1,
+	name: 'anyViewName',
+	index: 1,
+	roll: 20.91039276,
+	pitch: 5.58994007,
+	heading: 179.98199463,
+	cameraPositionId: 1,
+	url: 'https://anyurl.com',
+	worldId: 'Earth1',
+};
+
+export const ProjectModel = types.model('project_model', {
+	id: '1_project',
+	viewStore: types.maybe(ViewStoreModel)
+});
+
+const project = ProjectModel.create();
+//test
+createConnection({
+	type: 'sqlite',
+	database: ':memory:',
+	synchronize: true,
+	logging: true,
+	entities: [ViewEntity, LayerEntity, LisObjectEntity],
+}).then( async () => {
+	await getManager().transaction(async transactionEntityManager => {
+		project.viewStore && await project.viewStore.addItem(DEFAULT_VIEW, transactionEntityManager)
+	})
+});
+
--- projectRoot/stores/Project/views/ViewStore.ts
+++ projectRoot/stores/Project/views/ViewStore.ts
@@ -2,9 +2,14 @@ import createBaseTableModel from "../createBaseTableModel";
 import BaseRepository from "../BaseRepository";
 import { ViewModel } from "./ViewModel";
 import { ViewEntity } from "./ViewEntity";
-import { EntityRepository } from "typeorm";
+import { EntityRepository} from "typeorm";
 import { Instance} from "mobx-state-tree";
 
 @EntityRepository(ViewEntity)
 class ViewStoreRepo extends BaseRepository<ViewEntity, Instance<typeof ViewModel> & Record<string, any>> {}
-export const viewStoreModel = createBaseTableModel(ViewModel, ViewStoreRepo);
+
+export const ViewStoreModel = createBaseTableModel(ViewModel, ViewStoreRepo);
+
+
+const store = ViewStoreModel.create()
+console.log(store)
